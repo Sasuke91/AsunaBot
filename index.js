e0c90a75a73a9d63da772d29d053ff8d4c0e21c8
Universe <williamsthehack@gmail.com>
2022-12-08 20:04:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ process.on('uncaughtException', err => {
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { executablePath: '/usr/bin/google-chrome-stable', headless: true }
+    puppeteer: { executablePath: '/usr/bin/google-chrome-stable', headless: false, 	args: ['--no-sandbox'], }
 });
+
 
 client.on('qr', (qr) => {
     // NOTE: This event will not be fired if a session is specified.
