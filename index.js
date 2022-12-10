3a2bf960397f31ca26124deed668241e0894cd76
Universe <williamsthehack@gmail.com>
2022-12-10 02:09:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ process.on('uncaughtException', err => {
     console.error(err && err.stack)
 });
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
-
 const client = new Client({
   authStrategy: new LocalAuth(),
 });
