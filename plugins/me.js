63176c8bcca24ef467a0d6437628039f52fb6ec9
Universe <williamsthehack@gmail.com>
2022-12-10 03:38:19 +0100

    Update me.js

--- plugins/me.js
+++ plugins/me.js
@@ -13,28 +13,27 @@ module.exports = {
         var user = require('./user.js');
         var asuna = require('./asuna');
 
-
         if (args.length < 1) {
 
             user.details(msg, function(user){
                 // returns false or user
                 if (user != false) {
-                    asuna.format(user.date, function(date){
-                        // returns formated date
-                        msg.reply(
-                            "> "+user.username+" <"
-                            +"\n\n_"+user.bio+"_"
-                            +"\n\n> "+user.coins+"$ > "+user.xp+"☆"
-                            +"\n> "+user.style+" > "+user.deviceType
-                            +"\n> "+user.messages+" commands"
-                            +"\n> "+user.clearnumber
-                            +"\n> "+user.status+" [id "+user.user_id+"]"
-                            +"\n> "+user.gartic+" > "+user.gartic_points
-                            +"\n> "+user.slot+" > "+user.slot_points
-                            +"\n\n_star since "+date+"_"
-                        )
-                    });
-                }
+                asuna.format(user.date, function(date){
+                    // returns formated date
+                    msg.reply(
+                        "> "+user.username+" <"
+                        +"\n\n_"+user.bio+"_"
+                        +"\n\n> "+user.coins+"$ > "+user.xp+"☆"
+                        +"\n> "+user.style+" > "+user.deviceType
+                        +"\n> "+user.messages+" commands"
+                        +"\n> "+user.clearnumber
+                        +"\n> "+user.status+" [id "+user.user_id+"]"
+                        +"\n> "+user.gartic+" > "+user.gartic_points
+                        +"\n> "+user.slot+" > "+user.slot_points
+                        +"\n\n_star since "+date+"_"
+                    )
+                });
+            }
             });
 
         } else {
