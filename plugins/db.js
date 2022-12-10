e93b669729f4018cbe2c63ca0ade982efaecfc5e
Universe <williamsthehack@gmail.com>
2022-12-10 17:19:08 +0100

    Update db.js

--- plugins/db.js
+++ plugins/db.js
@@ -1,6 +1,6 @@
 mysql = require('mysql'); // Database connection
 
-var dbConnectionInfo = mysql.createPool({
+var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'johannw2004',
@@ -9,23 +9,5 @@ var dbConnectionInfo = mysql.createPool({
     dateStrings: true
 });
 
-//create mysql connection pool
-var dbconnection = mysql.createPool(
-    dbConnectionInfo
-  );
-  
-  // Attempt to catch disconnects 
-  dbconnection.on('connection', function (connection) {
-    console.log('DB Connection established');
-  
-    connection.on('error', function (err) {
-      console.error(new Date(), 'MySQL error', err.code);
-    });
-    connection.on('close', function (err) {
-      console.error(new Date(), 'MySQL close', err);
-    });
-  
-  });
-  
-  
-  module.exports = dbconnection;
+connection.connect();
+module.exports = connection;
