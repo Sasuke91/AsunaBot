80f6e5e5791717fd0d19b77565f558a1455b5944
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-12-12 16:23:16 +0100

    Update me.js

--- plugins/me.js
+++ plugins/me.js
@@ -23,7 +23,7 @@ module.exports = {
                 +"\n︱ os: "+user.deviceType
                 +"\n︱ commands: "+user.messages
                 +"\n︱ +"+user.clearnumber
-                +"\n︱ status: "+user.status+" 
+                +"\n︱ status: "+user.status 
                 +"\n︱ [id "+user.user_id+"]"
                 +"\n︱ gartic: "+user.gartic_points
                 +"\n︱ slot: "+user.slot_points
