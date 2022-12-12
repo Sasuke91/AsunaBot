be7029734b796bdea6ef43fd09015e47d585da62
Universe <williamsthehack@gmail.com>
2022-12-12 16:20:37 +0100

    Update me.js

--- plugins/me.js
+++ plugins/me.js
@@ -15,16 +15,19 @@ module.exports = {
         asuna.format(user.date, function(date){
             // returns formated date
             msg.reply(
-                "☬ "+user.username+" ☬"
-                +"\n\n⋮ _"+user.bio+"_"
-                +"\n\n⋮ "+user.coins+"$ > "+user.xp+"☆"
-                +"\n⋮ "+user.style+" > "+user.deviceType
-                +"\n⋮ "+user.messages+" commands"
-                +"\n⋮ "+user.clearnumber
-                +"\n⋮ "+user.status+" [id "+user.user_id+"]"
-                +"\n⋮ gartic > "+user.gartic_points
-                +"\n⋮ slot > "+user.slot_points
-                +"\n\n_➳ star since "+date+"_"
+                "❄ "+user.username+" ❄"
+                +"\n\n_"+user.bio+"_"
+                +"\n\n︱ "+user.coins+"$"
+                +"\n︱ xp: "+user.xp
+                +"\n︱ style: "+user.style
+                +"\n︱ os: "+user.deviceType
+                +"\n︱ commands: "+user.messages
+                +"\n︱ +"+user.clearnumber
+                +"\n︱ status: "+user.status+" 
+                +"\n︱ [id "+user.user_id+"]"
+                +"\n︱ gartic: "+user.gartic_points
+                +"\n︱ slot: "+user.slot_points
+                +"\n\n_⋆ star since "+date+"_"
             )
         });
         
